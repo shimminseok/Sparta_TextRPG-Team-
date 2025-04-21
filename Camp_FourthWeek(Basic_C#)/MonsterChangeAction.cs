b2439eb5da575630d8c7898c4767973811449fa0
KimgiChan07 <wekorean0727@naver.com>
2025-04-22 01:41:02 +0900

    feat: MosterChangeAction.sc 생성

---
+++ Camp_FourthWeek(Basic_C#)/MonsterChangeAction.cs
@@ -0,0 +1,20 @@
+﻿namespace Camp_FourthWeek_Basic_C__;
+
+
+public class MonsterChangeAction : ActionBase
+{
+    public override string Name => "포켓몬 박스";
+    public MonsterChangeAction(IAction _prevAction)
+    {
+        PrevAction = _prevAction;
+    }
+
+    public override void OnExcute()
+    {
+        Console.WriteLine("포켓몬을 관리 할 수 있습니다.");
+        Console.WriteLine("\n[포켓몬 목록]");
+
+
+        SelectAndRunAction(SubActionMap);
+    }
+}
