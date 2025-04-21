b821c5df06577d0a47660fb0ed0987b9dea540bb
shimminseok <61370379+shimminseok@users.noreply.github.com>
2025-04-21 19:03:09 +0900

    feat : EnterQuestAction 추가
    - EnterQuestAction 추가

---
+++ Camp_FourthWeek(Basic_C#)/EnterQuestAction.cs
@@ -0,0 +1,10 @@
+namespace Camp_FourthWeek_Basic_C__;
+
+public class EnterQuestAction : ActionBase
+{
+    public override string Name => "퀘스트";
+
+    public override void OnExcute()
+    {
+    }
+}
