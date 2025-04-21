1446611d50855325c77c9d7d1301dc4f13b43bd0
miju99 <mji0@naver.com>
2025-04-22 01:53:48 +0900

    feat : EnterBattleasic.cs
    - EnterBattleasic 기능 추가

--- Camp_FourthWeek(Basic_C#)/EnterBattleBasic .cs
+++ Camp_FourthWeek(Basic_C#)/EnterBattleBasic .cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,25 @@ namespace Camp_FourthWeek_Basic_C__
 
         public override void OnExcute()
         {
+            Console.WriteLine("Battle!!");
+
+            List<Monster> MonstersAllList = MonsterTable.MonsterDataDic.Values.ToList();
+            List<Monster> MonsterSelectList = new List<Monster>();
+
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 5);
+            for (int i = 0; i < randomNum; i++)
+            {
+                int randomMonsterNum = rand.Next(0, MonsterSelectList.Count);
+                MonsterSelectList.Add(MonstersAllList[randomMonsterNum]);
+            }
+
+            foreach (Monster monster in MonsterSelectList)
+            {
+                float maxHP = monster.Stats[StatType.MaxHp].
+                Console.WriteLine($"{monster.Name} HP {maxHP}"); //미주야 아침에 고쳐야 해!
+            }
+
             //몬스터 1~4마리 랜덤 등장
             //몬스터 정보 - LV 이름 HP ATK
 
