ba3dda8874b17619c18bdf14a4341006c2167166
KimgiChan07 <wekorean0727@naver.com>
2025-04-22 11:32:04 +0900

    feat: 포켓몬 목록 포켓몬 호출

--- Camp_FourthWeek(Basic_C#)/MonsterBoxAction.cs
+++ Camp_FourthWeek(Basic_C#)/MonsterBoxAction.cs
@@ -18,7 +18,20 @@ public class MonsterBoxAction : ActionBase
         Console.WriteLine("포켓몬을 관리 할 수 있습니다.");
         Console.WriteLine("\n[포켓몬 목록]");
         
-        
+        var player = GameManager.Instance.PlayerInfo;
+
+        foreach (var m in player.Monsters)
+        {
+            var isEquipped = (m == player.Monster) ? "[E]" : " ";
+            var name = m.Name;
+            var level = m.Lv;
+            var hp = m.Stats[StatType.CurHp].FinalValue;
+            var mp = m.Stats[StatType.CurMp].FinalValue;
+            var itemName = m.ItemId != 0 ? ItemTable.GetItemById(m.ItemId)?.Name ?? "알수 없음" : "없음";
+
+            Console.WriteLine($"{isEquipped}{name,-10} | LV {level} | HP {hp} / MP {mp} | 도구{itemName}");
+
+        }
         
         
         SelectAndRunAction(SubActionMap);
