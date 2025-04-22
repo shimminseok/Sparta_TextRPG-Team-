34a177b4005bff771b5689ca844150d5f4e53620
KimgiChan07 <wekorean0727@naver.com>
2025-04-22 10:32:48 +0900

    refactor: 포켓몬 관리와 박스를 분할

---
+++ Camp_FourthWeek(Basic_C#)/MonsterBoxAction.cs
@@ -0,0 +1,26 @@
+﻿namespace Camp_FourthWeek_Basic_C__;
+
+
+public class MonsterBoxAction : ActionBase
+{
+    public override string Name => "포켓몬 박스";
+    public MonsterBoxAction(IAction _prevAction)
+    {
+        PrevAction = _prevAction;
+        SubActionMap = new Dictionary<int, IAction>
+        {
+            { 1, new MonsterChangeAction(this) }
+        };
+    }
+
+    public override void OnExcute()
+    {
+        Console.WriteLine("포켓몬을 관리 할 수 있습니다.");
+        Console.WriteLine("\n[포켓몬 목록]");
+        
+        
+        
+        
+        SelectAndRunAction(SubActionMap);
+    }
+}
--- Camp_FourthWeek(Basic_C#)/MonsterChangeAction.cs
+++ Camp_FourthWeek(Basic_C#)/MonsterChangeAction.cs
@@ -1,20 +1,23 @@
-﻿namespace Camp_FourthWeek_Basic_C__;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-public class MonsterChangeAction : ActionBase
+namespace Camp_FourthWeek_Basic_C__;
+class MonsterChangeAction : ActionBase
 {
-    public override string Name => "포켓몬 박스";
+    public override string Name => "포켓몬 관리";
     public MonsterChangeAction(IAction _prevAction)
     {
         PrevAction = _prevAction;
     }
-
     public override void OnExcute()
     {
-        Console.WriteLine("포켓몬을 관리 할 수 있습니다.");
-        Console.WriteLine("\n[포켓몬 목록]");
-
-
+        Console.WriteLine();
+        Console.WriteLine("[포켓몬 목록]");
+        
+        
         SelectAndRunAction(SubActionMap);
     }
 }
