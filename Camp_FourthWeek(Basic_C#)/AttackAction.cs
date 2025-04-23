a68db4d28937c5fc665aeffb19488ad09aa8e843
miju99 <mji0@naver.com>
2025-04-24 08:50:33 +0900

    feat : AttackAction.cs
    - AttackAction.cs 공격력 오차범위 계산

--- Camp_FourthWeek(Basic_C#)/AttackAction.cs
+++ Camp_FourthWeek(Basic_C#)/AttackAction.cs
@@ -14,15 +14,24 @@ namespace Camp_FourthWeek_Basic_C__
 
         public override void OnExcute()
         {
-            float damage = PlayerInfo.Monster.Stats[StatType.Attack].FinalValue;
+            float originalDamage = PlayerInfo.Monster.Stats[StatType.Attack].FinalValue;
+            float minDamage = originalDamage * 0.9f; //공격력의 최소값
+            float maxDamage = originalDamage * 1.1f; //공격력의 최대값
+
+            Random random = new Random();
+            float randomNum = (float)random.NextDouble() * (maxDamage - minDamage); //최소값과 최대값 사이에서 랜덤 값 구하기
+            float roundedValue = (float)Math.Round(randomNum, 0); //소수점 1자리에서 반올림
+
+            float damage = maxDamage - roundedValue; //공격력에서 10% 오차가 생긴 데미지
+
             float monsterOriginHp = monster.Stats[StatType.CurHp].FinalValue;
 
             //메세지 수정 -> 
             Console.WriteLine("Battle");
             Console.WriteLine($"{PlayerInfo.Monster.Name}의 공격!");
-            Console.WriteLine($"Lv.{monster.Lv} {monster.Name}을(를) 맞췄습니다. [데미지 : {damage}\n");
+            Console.WriteLine($"Lv.{monster.Lv} {monster.Name}을(를) 맞췄습니다. [데미지 : {damage}]\n");
             Console.WriteLine($"Lv.{monster.Lv} {monster.Name}");
-
+            
             //여기서 공격
             monster.Stats[StatType.CurHp].ModifyAllValue(damage);
 
