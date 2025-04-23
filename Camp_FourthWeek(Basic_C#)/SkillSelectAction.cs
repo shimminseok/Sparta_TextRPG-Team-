07151749a5c61f0a77f3b9d9c573eb2740cf36f9
younv2 <younv2@naver.com>
2025-04-23 19:45:12 +0900

    feat : SkillSelectAction 추가

---
+++ Camp_FourthWeek(Basic_C#)/SkillSelectAction.cs
@@ -0,0 +1,35 @@
+﻿namespace Camp_FourthWeek_Basic_C__
+{
+
+    public class SkillSelectAction : ActionBase
+    {
+        private List<Skill> skills = new();
+
+        public SkillSelectAction(IAction _prevAction)
+        {
+            PrevAction = _prevAction;
+        }
+
+        public override string Name => $"스킬 선택";
+
+        public override void OnExcute()
+        {
+            skills = SkillTable.SkillDataDic.Values
+            .Where(item => PlayerInfo.Skills.Contains(item.Id))
+            .ToList();
+            var message = string.Empty;
+            for (int i = 0; i < skills.Count; i++)
+            {
+                //if (!SubActionMap.ContainsKey(i + 1))
+                   // SubActionMap.Add(i + 1, new AttackSelectAction(this, skills[i]));
+            }
+
+            SelectAndRunAction(SubActionMap);
+            if (PrevAction != null)
+            {
+                PrevAction.SetFeedBackMessage(message);
+                PrevAction.Execute();
+            }
+        }
+    }
+}
