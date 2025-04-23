9911a61c546efdfb9849a33468b8d0f50acdf97e
shimminseok <61370379+shimminseok@users.noreply.github.com>
2025-04-23 16:12:23 +0900

    feat : StageTest 추가
    -StageTest 추가

--- Camp_FourthWeek(Basic_C#)/StageAction.cs
+++ Camp_FourthWeek(Basic_C#)/StageAction.cs
@@ -2,25 +2,50 @@ namespace Camp_FourthWeek_Basic_C__;
 
 public class StageAction : ActionBase
 {
+    public override string Name => stage.StageName;
     private readonly Stage? stage;
 
-    public StageAction(int _dunNum, IAction _prevAction)
+    public StageAction(int _key, IAction _prevAction)
     {
-        stage = StageTable.GetDungeonById(_dunNum);
         PrevAction = _prevAction;
+        stage = StageTable.GetDungeonById(_key);
     }
 
-    public override string Name => stage.StageName;
 
     public override void OnExcute()
     {
         //전투 시작
-        //내가 이기면
-        StageManager.Instance.ClearCurrentStage(stage);
+        TestBattleAction battle = new TestBattleAction(stage.Key, this);
+        battle.OnExcute();
+        SelectAndRunAction(SubActionMap);
+        // PrevAction!.Execute();
+    }
+}
 
-        //내가 지면
-        StageManager.Instance.UnClearStage(stage);
+public class TestBattleAction : ActionBase
+{
+    public override string Name => "전투 시작";
+    private Stage stage;
+    private Random random = new Random();
 
-        PrevAction!.Execute();
+    public TestBattleAction(int _stageNum, IAction _prevAction)
+    {
+        stage = StageTable.GetDungeonById(_stageNum);
+        PrevAction = _prevAction;
+    }
+
+    public override void OnExcute()
+    {
+        //몬스터를 뿌려주고
+        int monsterCount = random.Next(1, 5);
+        for (int i = 0; i < monsterCount; i++)
+        {
+            int monsterIndex = random.Next(0, stage.SpawnedMonsters.Length);
+            MonsterType monsterKey = stage.SpawnedMonsters[monsterIndex];
+            Monster monster = MonsterTable.GetMonsterByType(monsterKey);
+            Console.WriteLine($"{i + 1}. {monster.Name}");
+
+            CollectionManager.Instnace.OnDiscovered(monsterKey);
+        }
     }
 }
