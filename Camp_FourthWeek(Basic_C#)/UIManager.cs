1fc45bfeb4ee291717200ef77a318463d5a0e565
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-04-23 13:54:18 +0900

    feat : UI의 특정 위치에 원하는 문자열 등록 기능 추가
    - UI 특정 위치에 아스키 아트, 혹은 text를 출력하는 기능을 추가하였습니다.

--- Camp_FourthWeek(Basic_C#)/UIManager.cs
+++ Camp_FourthWeek(Basic_C#)/UIManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,33 +43,51 @@ namespace Camp_FourthWeek_Basic_C__
             {
                 string placeholder = "{" + pair.Key + "}";
                 int index = template.IndexOf(placeholder);
-
                 if (index < 0) continue;
 
                 string replacement = pair.Value;
 
-                // 한글 수 계산
+                // placeholder 길이 대비 replacement의 “폭” 계산
                 int deleteCount = -placeholder.Length;
                 foreach (char c in replacement)
                 {
-                    if (IsKorean(c)) deleteCount += 2;
-                    else deleteCount += 1;
+                    deleteCount += IsKorean(c) ? 2 : 1;
                 }
 
                 // 치환
                 template = template.Replace(placeholder, replacement);
 
-                // 치환된 값 뒤에서 공백 제거
+                // replacement 끝 위치
                 int afterIndex = index + replacement.Length;
-                int removed = 0;
-                while (afterIndex < template.Length && removed < deleteCount && template[afterIndex] == ' ')
+
+                if (deleteCount > 0)
                 {
-                    template = template.Remove(afterIndex, 1);
-                    removed++;
+                    // 남는 폭만큼 뒤 공백 제거
+                    int removed = 0;
+                    while (afterIndex < template.Length && removed < deleteCount && template[afterIndex] == ' ')
+                    {
+                        template = template.Remove(afterIndex, 1);
+                        removed++;
+                    }
+                }
+                else if (deleteCount < 0)
+                {
+                    // 부족한 폭만큼 뒤에 공백 추가
+                    int padCount = -deleteCount;
+                    template = template.Insert(afterIndex, new string(' ', padCount));
                 }
             }
 
             return template;
+        }
+
+        public static Dictionary<string, string> MergeFormatDic(Dictionary<string, string> A, Dictionary<string, string> B)
+        {
+            foreach (var value in B)
+            {
+                A[value.Key] = value.Value;
+            }
+            return A;
         }
     }
     
@@ -81,10 +100,43 @@ namespace Camp_FourthWeek_Basic_C__
 
         private static char[,] uiPanel = new char[height, width];
         private static Dictionary<int, List<int>> uiDic = new Dictionary<int, List<int>>();
-        private static Tuple<int, int> cusorPosition;
+        private static Tuple<int, int>? cusorPosition;
         public static void TextShowUI()
         {
-              UIUpdater(UIName.Intro_TextBox);
+           /*
+           if (IntroRoop() == 1)
+            {
+                
+                UIUpdater(UIName.Intro_TextBox);
+                    UIUpdater(UIName.Intro_SetStarting, new Dictionary<int, Tuple<int, int>?>
+                {                
+                    {0, new Tuple<int, int>(0,10) },
+                    {1, new Tuple<int, int>(20,10) },
+                    {2, new Tuple<int, int>(70,10) },
+                    {3, new Tuple<int, int>(120,10) },
+
+                });
+                 UIUpdater(UIName.Main);
+            }
+           
+            */
+            UIUpdater(UIName.Status, new Dictionary<int, Tuple<int, int>?>
+                {
+                    {0, new Tuple<int, int>(0,0) },
+                    {1, new Tuple<int, int>(10,21)},
+                },
+                new Dictionary<string, string>
+                {
+                    {"13", "한지우"},
+                    {"14", "1500G"},
+                    {"15", "피카츄"},
+                    {"16", "1"},
+                    {"17", "10"},
+                    {"18", "5"},
+                    {"19", "100"},
+                    {"20", "200"},
+                });
+
         }
         public static void PrintPanel()
         {
@@ -103,88 +155,87 @@ namespace Camp_FourthWeek_Basic_C__
             Console.Write(sb.ToString());
         }
 
-        public static void UIUpdater(UIName name)
+        public static void UIUpdater(UIName name, Dictionary<int, Tuple<int, int>?>? fixPivotDic = null, Dictionary<string, string>? addFormat = null)
         {
             Console.Clear();
+            ResetUIPanel();
 
             var printUI = UITable.UITableDic[name];
-            foreach (var value in printUI)
-            {
-                ChangeUiPanel(name, UITable.UIDic[value]);
-            }
+            int dicCount = 0;
 
+            if(fixPivotDic != null)
+            {
+                foreach (int value in printUI)
+                {
+                    if (printUI[dicCount] < 100)
+                    {
+                        ChangeUiPanel(name, UITable.UIDic[value], null, addFormat);
+                        dicCount++;
+                    }
+                    else
+                    {
+                        ChangeUiPanel(name, UITable.UIDic[value], fixPivotDic[dicCount], addFormat);
+                        dicCount++;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var value in printUI)
+                {
+                    ChangeUiPanel(name, UITable.UIDic[value]);
+                }
+            }
             // 출력
             PrintPanel();
 
             // 커서 위치 변경
-            ReadLineAt(cusorPosition.Item1, cusorPosition.Item2);
+            if(cusorPosition!=null)
+                ReadLineAt(cusorPosition.Item1, cusorPosition.Item2);
         }
-public static void ReadLineAt(int left, int top)
-{
-    string inputBuffer = "";
-    int displayWidth = 0;
-
-    while (true)
-    {
-        ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
-
-        if (keyInfo.Key == ConsoleKey.Enter)
-            break;
-
-        if (keyInfo.Key == ConsoleKey.Backspace)
+  
+        public static void ChangeUiPanel(UIName name, UI ui, Tuple<int,int>? fixedPivot = null, Dictionary<string, string>? addFormat = null)
         {
-            if (inputBuffer.Length > 0)
+            int pivotX = 0;
+            int pivotY = 0;
+            if (fixedPivot != null)
             {
-                char lastChar = inputBuffer[^1];
-                int charWidth = GetCharWidth(lastChar);
-                displayWidth -= charWidth;
-                inputBuffer = inputBuffer.Substring(0, inputBuffer.Length - 1);
-
-                for (int i = 0; i < charWidth; i++)
-                    uiPanel[height - top, left + displayWidth + i] = ' ';
+               ( pivotX,  pivotY) = fixedPivot;
+                pivotY -= 1;
             }
-        }
-        else if (!char.IsControl(keyInfo.KeyChar))
-        {
-            char c = keyInfo.KeyChar;
-            int charWidth = GetCharWidth(c);
-
-            if (left + displayWidth + charWidth <= width) // overflow 방지
+            else
             {
-                inputBuffer += c;
-                uiPanel[height - top, left + displayWidth] = c;
-
-                if (charWidth == 2)
-                    uiPanel[height - top, left + displayWidth + 1] = ' ';
-
-                displayWidth += charWidth;
+                ( pivotX,  pivotY) = ui.Pivot;
+                pivotY -= 1;
             }
-        }
-
-        PrintPanel();
-        Console.SetCursorPosition(left + displayWidth, height - top);
-    }
-}
-
-
-
-        public static void ChangeUiPanel(UIName name, UI ui)
-        {
-            (int pivotX, int pivotY) = ui.Pivot;
-            pivotY -=1;
-
             if(ui.CusorPivot.Item1 != 0 && ui.CusorPivot.Item2 != 0)
             {
                 cusorPosition = ui.CusorPivot;
             }
             string inputString = ui.UiString;
-            string changeString = FormatText(inputString, UITable.textDic[name]);
+            string changeString;
+            if (UITable.textDic.ContainsKey(name))
+            {
+                if(addFormat == null)
+                {
+                    changeString = FormatText(inputString, UITable.textDic[name]);
+                }
+                else
+                {
+                    var mergeDic = MergeFormatDic(UITable.textDic[name], addFormat);
+                    changeString = FormatText(inputString, mergeDic);
+                }
+
+             
+            }
+            else
+            {
+                changeString = inputString;
+
+            }
             string[] splitStrings = changeString.Split('\n');
 
-
-
-
-            for (int y = 0; y < splitStrings.Length; y++)
+            for (int y = 1; y < splitStrings.Length; y++)
             {
                 string line = splitStrings[y];
 
@@ -198,12 +249,107 @@ public static void ReadLineAt(int left, int top)
                     {
                         continue;
                     }
+                    if(line[x] == '$')
+                    {
+                        continue;
+                    }
 
                     uiPanel[panelY, panelX] = line[x];
                 }
             }
         }
+        public static void ReadLineAt(int left, int top)
+        {
+            string inputBuffer = "";
+            int displayWidth = 0;
 
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                    break;
+
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (inputBuffer.Length > 0)
+                    {
+                        char lastChar = inputBuffer[^1];
+                        int charWidth = GetCharWidth(lastChar);
+                        displayWidth -= charWidth;
+                        inputBuffer = inputBuffer.Substring(0, inputBuffer.Length - 1);
+
+                        for (int i = 0; i < charWidth; i++)
+                            uiPanel[height - top, left + displayWidth + i] = ' ';
+                    }
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    char c = keyInfo.KeyChar;
+                    int charWidth = GetCharWidth(c);
+
+                    if (left + displayWidth + charWidth <= width) // overflow 방지
+                    {
+                        inputBuffer += c;
+                        uiPanel[height - top, left + displayWidth] = c;
+
+                        if (charWidth == 2)
+                            uiPanel[height - top, left + displayWidth + 1] = ' ';
+
+                        displayWidth += charWidth;
+                    }
+                }
+
+                PrintPanel();
+                Console.SetCursorPosition(left + displayWidth, height - top);
+            }
+        }
+        public static int IntroRoop()
+        {
+            while (true)
+            {
+                UIUpdater(UIName.Intro_GameStart_1);
+                Thread.Sleep(200);
+                var result = CheckIntroInput(); if (result.HasValue) return result.Value;
+
+                UIUpdater(UIName.Intro_GameStart_2);
+                Thread.Sleep(200);
+                result = CheckIntroInput(); if (result.HasValue) return result.Value;
+
+                UIUpdater(UIName.Intro_GameStart_3);
+                Thread.Sleep(200);
+                result = CheckIntroInput(); if (result.HasValue) return result.Value;
+
+                UIUpdater(UIName.Intro_GameStart_2);
+                Thread.Sleep(200);
+                result = CheckIntroInput(); if (result.HasValue) return result.Value;
+
+                UIUpdater(UIName.Intro_GameStart_1);
+                Thread.Sleep(200);
+                result = CheckIntroInput(); if (result.HasValue) return result.Value;
+
+                UIUpdater(UIName.Intro_GameStart_4);
+                Thread.Sleep(200);
+                result = CheckIntroInput(); if (result.HasValue) return result.Value;
+            }
+        }
+
+        public static int? CheckIntroInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.D1) return 1;
+                if (key == ConsoleKey.D0) return 0;
+            }
+
+            return null;
+        }
+        public static void ResetUIPanel()
+        {
+            uiPanel = new char[height, width];
+        }
 
 
         public static StringBuilder ItemPrinter(Item _item, int _index = -1, bool _isShowDescript = true)
