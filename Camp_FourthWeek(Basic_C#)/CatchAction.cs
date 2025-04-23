52ef51e4ca4efc7bdfffcefcc3ee5f836d9369d7
younv2 <younv2@naver.com>
2025-04-23 21:39:02 +0900

    feat : 포획 기능 보완

--- Camp_FourthWeek(Basic_C#)/CatchAction.cs
+++ Camp_FourthWeek(Basic_C#)/CatchAction.cs
@@ -18,16 +18,24 @@ namespace Camp_FourthWeek_Basic_C__
         {
             float basicCatchChance = 0.3f;
             float maximumCatchChance = 0.9f;
-
+            Random rand = new Random(DateTime.Now.Millisecond);
             float hpRatio = monster.Stats[StatType.CurHp].FinalValue / monster.Stats[StatType.MaxHp].FinalValue;
             float catchChance = maximumCatchChance - (maximumCatchChance - basicCatchChance) * hpRatio;
 
+            if(rand.NextDouble()<catchChance)
+            {
+                QuestManager.Instance.UpdateCurrentCount((QuestTargetType.Monster, QuestConditionType.Catch),
+                    (int)monster.Type);
+                InventoryManager.Instance.AddMonsterToBox(monster);
+                
+                Console.WriteLine("포획 성공!");
+            }
+            else
+            {
+                Console.WriteLine("포획 실패............................");
 
-            //포획 성공시
-            QuestManager.Instance.UpdateCurrentCount((QuestTargetType.Monster, QuestConditionType.Catch),
-                (int)monster.Type);
-            InventoryManager.Instance.AddMonsterToBox(monster);
-            //
+            }
+
 
             SelectAndRunAction(SubActionMap);
         }
