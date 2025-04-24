109c1ad3e10b89bd7748b47abeffa1431adb208e
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-04-24 21:01:43 +0900

    feat : 캐릭터 스탯창 UI 연동 완료
    - 캐릭터 스탯창 UI 연동 완료
    - 추후 리스트 관련 UI는 전부 재연동 필요

--- Camp_FourthWeek(Basic_C#)/EnterCharacterInfoAction.cs
+++ Camp_FourthWeek(Basic_C#)/EnterCharacterInfoAction.cs
@@ -4,6 +4,9 @@ using static Camp_FourthWeek_Basic_C__.StringUtil;
 
 public class EnterCharacterInfoAction : ActionBase
 {
+    int addId = 99;
+   
+    Tuple<int, int> monsterPivot = new Tuple<int, int>(10, 23);
     public EnterCharacterInfoAction(IAction _prevAction)
     {
         PrevAction = _prevAction;
@@ -13,22 +16,26 @@ public class EnterCharacterInfoAction : ActionBase
 
     public override void OnExcute()
     {
-        Console.WriteLine("캐릭터의 정보가 표시됩니다.");
-        Console.WriteLine();
+        int dicNum = 13;
+        Dictionary<int, string> stringDic = new Dictionary<int, string>();
+        int index = Array.IndexOf(Enum.GetValues(typeof(MonsterType)), PlayerInfo.Monster.Type) + addId; 
+        var pivotDic =  new Dictionary<int, Tuple<int, int>?>
+                  {
+                      {0, new Tuple<int, int>(0,0) },
+                      {1, monsterPivot},
+                  };
+        var myMonsterList = new List<int>() { index };
 
-        Console.WriteLine($"{PadRightWithKorean("트레이너", 12)} : {PlayerInfo.Name}");
-        Console.WriteLine($"{PadRightWithKorean("Gold", 12)} : {PlayerInfo.Gold} G");
-        Console.WriteLine();
-        Console.WriteLine("[장착중인 포켓몬]");
-        Console.WriteLine(PadRightWithKorean($"{PadRightWithKorean("포켓몬", 12)} : {PlayerInfo.Monster.Name}", 10));
-        Console.WriteLine($"{PadRightWithKorean("Lv.", 12)} : {LevelManager.CurrentLevel}");
-        Console.WriteLine();
-        Console.WriteLine("[포켓몬 능력치]");
+        stringDic.Add(dicNum++, PlayerInfo.Name);
+        stringDic.Add(dicNum++, PlayerInfo.Gold.ToString());
+        stringDic.Add(dicNum++, PlayerInfo.Monster.Name);
+        stringDic.Add(dicNum++, PlayerInfo.Monster.Lv.ToString());
+
         foreach (var stat in PlayerInfo.Monster.Stats.Values)
-            Console.WriteLine(
-                $"{PadRightWithKorean(stat.GetStatName(), 12)} : {PadRightWithKorean(stat.FinalValue.ToString("N0"), 9)} +({stat.EquipmentValue})");
+        {
+            stringDic.Add(dicNum++, stat.FinalValue.ToString());
+        }
 
-
-        SelectAndRunAction(SubActionMap);
+        SelectAndRunAction(SubActionMap,false, () => UiManager.UIUpdater(UIName.Status, pivotDic, (10, stringDic), myMonsterList));
     }
 }
--- Camp_FourthWeek(Basic_C#)/PagedListActionBase.cs
+++ Camp_FourthWeek(Basic_C#)/PagedListActionBase.cs
@@ -4,8 +4,8 @@ public abstract class PagedListActionBase : ActionBase
 {
     protected const int VIEW_COUNT = 3;
     protected int Page;
-    protected int lineCount = 5;
     protected int MaxPage;
+    protected int LineCount = 5;
     protected bool isViewSubMap = true;
     protected abstract List<string> GetPageContent();
     protected abstract PagedListActionBase CreateNew(int newPage);
@@ -19,26 +19,26 @@ public abstract class PagedListActionBase : ActionBase
 
     public override void OnExcute()
     {
-        // SubActionMap.Clear();
-
+        // 이 부분 나중에 지우고, 
         var lines = GetPageContent();
-        Dictionary<int, string> lineDic = new Dictionary<int, string>();
-        for(int i = 0; i < lines.Count; i++)
+        foreach (var line in lines)
         {
-            lineDic.Add(lineCount + i, lines[i]);
+            Console.WriteLine(line);
         }
 
+        Console.WriteLine();
         Console.WriteLine($"[{Page + 1}/{MaxPage}] 페이지");
-         Console.WriteLine();
+        Console.WriteLine();
 
-       if (Page > 0)
-            lineDic.Add(8,"-1. 이전 페이지");
+        if (Page > 0)
+            Console.WriteLine("-1. 이전 페이지");
         if (Page < MaxPage - 1)
-            lineDic.Add(9,"-2. 다음 페이지");
+            Console.WriteLine("-2. 다음 페이지");
+        //
 
-
-       PageNavigationFactory.AddPageNavigation(SubActionMap, p => CreateNew(p), Page, MaxPage);
-
-        SelectAndRunAction(SubActionMap, isViewSubMap,() => UiManager.UIUpdater(UIName.Collection, null, (5, lineDic)));
+        PageNavigationFactory.AddPageNavigation(SubActionMap, p => CreateNew(p), Page, MaxPage);
+        
+        // 이 아래도 지워야함
+        SelectAndRunAction(SubActionMap, isViewSubMap);
     }
 }
