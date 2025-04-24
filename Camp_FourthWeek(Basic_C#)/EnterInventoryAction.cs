4a17ea7600034b9cef130b44f3d94262bf408d41
shimminseok <61370379+shimminseok@users.noreply.github.com>
2025-04-24 12:55:13 +0900

    refactor : 도구 페이지 작업 수정
    - 도구 페이지 작업 수정

--- Camp_FourthWeek(Basic_C#)/EnterInventoryAction.cs
+++ Camp_FourthWeek(Basic_C#)/EnterInventoryAction.cs
@@ -2,16 +2,14 @@ namespace Camp_FourthWeek_Basic_C__;
 
 public class EnterInventoryAction : ActionBase
 {
-
     public EnterInventoryAction(IAction _prevAction)
     {
         PrevAction = _prevAction;
         SubActionMap = new Dictionary<int, IAction>
         {
-            { 1, new EquipItemManagementAction(this,GameManager.Instance.PlayerInfo,
-                                                InventoryManager.Instance) },
+            { 1, new EquipItemManagementAction(this) },
             { 2, new MonsterBoxAction(this) },
-            {3, new UseItemAction(this)},
+            { 3, new UseItemAction(this) },
         };
     }
 
